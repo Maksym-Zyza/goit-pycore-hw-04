3cfb59b658ef6781075bdade98e202e5d9b27194
Maksym-Zyza <mmziza88@gmail.com>
2025-03-14 21:13:02 +0200

    feat(task3): add display_dir_structure function

---
+++ 03/03.py
@@ -0,0 +1,22 @@
+import pathlib
+import colorama
+import os
+
+def display_dir_structure(path, level=0):
+    if not os.path.exists(path):
+        return print(f'File {path} does not exist')
+    
+    if not os.path.isdir(path):
+        return print(f'Path {path} is not a directory')
+    
+    path = pathlib.Path(path)
+    for item in path.iterdir():
+        if item.is_file():
+            print(colorama.Fore.GREEN + f'{"   " * level}{item.name}')
+        else:
+            print(colorama.Fore.BLUE + f'{"   " * level}{item.name}/')
+            display_dir_structure(item, level + 1)
+
+    return
+
+display_dir_structure("03")
